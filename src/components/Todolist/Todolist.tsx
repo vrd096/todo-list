37f6480cd24e59e25038689a105844a3c8864003
vrd096 <48715486+vrd096@users.noreply.github.com>
2023-05-16 23:37:17 +0500

    теперь стартует с /today

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,5 +1,5 @@
 import styles from './Todolist.module.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { MyDay } from '../../pages/MyDay';
 import { Important } from '../../pages/Important';
 import { Inbox } from '../../pages/Inbox';
@@ -8,6 +8,7 @@ export const Todolist = () => {
   return (
     <div className={styles.todo}>
       <Routes>
+        <Route path="/" element={<Navigate to="/today" />} />
         <Route path="/today" element={<MyDay />} />
         <Route path="/important" element={<Important />} />
         <Route path="/inbox" element={<Inbox />} />
