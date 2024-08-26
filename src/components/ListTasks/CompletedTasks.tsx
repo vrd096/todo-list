bf7e4644583fdd6a7938c3c287546f3a3fbeba8e
vrd096 <48715486+vrd096@users.noreply.github.com>
2024-08-26 21:13:55 +0500

    завершенные задачи в списке погружаются по 20 при прокрутке вниз, а не все разом

--- src/components/ListTasks/CompletedTasks.tsx
+++ src/components/ListTasks/CompletedTasks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { TaskActiveButton } from '../TaskActiveButton';
 import styles from './ListTasks.module.scss';
 import { Todo } from '../../redux/tasks/types';
@@ -12,6 +12,8 @@ interface CompletedTasksProps {
   callbackRemoveImportant: (task: Todo) => void;
 }
 
+const ITEMS_PER_PAGE = 20;
+
 const CompletedTasks: React.FC<CompletedTasksProps> = ({
   tasks,
   callbackChangeToActiveTask,
@@ -19,10 +21,53 @@ const CompletedTasks: React.FC<CompletedTasksProps> = ({
   callbackAddImportant,
   callbackRemoveImportant,
 }) => {
+  const [currentTasks, setCurrentTasks] = useState<Todo[]>([]);
+  const [page, setPage] = useState(1); // Start with the first page
+  const observer = useRef<IntersectionObserver | null>(null);
+  const lastTaskRef = useRef<HTMLLIElement | null>(null);
+
+  const loadMoreTasks = useCallback(() => {
+    if (currentTasks.length >= tasks.length) {
+      // Если все задачи загружены
+      //   console.log('All tasks loaded');
+      return;
+    }
+
+    const nextPage = page + 1;
+    const newTasks = tasks.slice(0, nextPage * ITEMS_PER_PAGE);
+    setCurrentTasks(newTasks);
+    setPage(nextPage);
+  }, [page, tasks, currentTasks]);
+
+  useEffect(() => {
+    const refElement = lastTaskRef.current;
+
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        loadMoreTasks();
+      }
+    });
+
+    if (refElement) {
+      observer.current.observe(refElement);
+    }
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [loadMoreTasks, currentTasks]);
+
+  useEffect(() => loadMoreTasks(), []); // Only load initially
+
   return (
     <ul className={styles.tasksList}>
-      {tasks.map((task) => (
-        <li className={styles.taskItem} key={task.id}>
+      {currentTasks.map((task, index) => (
+        <li
+          className={styles.taskItem}
+          key={task.id}
+          ref={index === currentTasks.length - 1 ? lastTaskRef : null}>
           <div className={styles.titleWrapper}>
             <TaskActiveButton callbackChangeToActiveTask={callbackChangeToActiveTask} task={task} />
             <button className={styles.taskTitleButton} onClick={() => openTaskDetails(task)}>
